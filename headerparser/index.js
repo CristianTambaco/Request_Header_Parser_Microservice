ae5900b2ccc4647caa25676c0efa69360d40e655
CristianTambaco <168015193+CristianTambaco@users.noreply.github.com>
2025-09-08 20:16:28 -0500

    Add files via upload

---
+++ headerparser/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// CORS para hacer requests
+app.use(cors());
+
+// Ruta principal opcional
+app.get('/', (req, res) => {
+  res.send('Microservicio de analizador de solicitud de encabezado');
+});
+
+// Ruta principal requerida por las pruebas
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress?.split(',')[0], // solo la IP original
+    language,
+    software
+  });
+});
+
+// Puerto de escucha
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
